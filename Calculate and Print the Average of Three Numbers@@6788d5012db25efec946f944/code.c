95936950afbd93925bd197fbf2570047454d0aa8
naisha-gurung <naisha.gurung03@gmail.com>
2025-01-17 20:47:37 +0530

    Solution to Calculate and Print the Average of Three Numbers

---
+++ Calculate and Print the Average of Three Numbers@@6788d5012db25efec946f944/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    scanf("%i",&a);
+    scanf("%i",&b);
+    scanf("%i",&c);
+    printf("Average: %.2f", (a+b+c)/3);
+    return 0;
+}
