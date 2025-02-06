3ecd882a428d5b41e79c8515475dafc436c1e2e1
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-06 17:05:37 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int arr[3];
+    for(int i=0;i<3;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<2;i++){
+        for(int j=i+1;j<3;j++){
+            if arr[i]+arr[j]<arr[3-(i+j)]{
+                printf("%s","Invalid");
+                return 0;
+            }
+        }
+
+
+    }
+    printf("%s","Valid");
+    return 0;
+}
