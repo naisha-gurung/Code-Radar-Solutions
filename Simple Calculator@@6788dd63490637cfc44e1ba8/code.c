62a5a43bb5abd91bf275722060b72cd7f221de6e
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-06 21:43:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,7 +4,7 @@ int main() {
     char c;
     scanf("%d %d %c",&a,&b,&c);
     if(c=="+"){
-        printf("%d",a+b);
+        // printf("%d",a+b);
     }
     // else if(c=="*"){
     //     printf("%d",a*b);
