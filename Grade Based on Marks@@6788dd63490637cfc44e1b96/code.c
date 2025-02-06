4f275b7ce57d0ee1a219869182016071cc035a58
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-06 20:31:31 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>=90){
+        printf("%c",'A');
+    }
+    else if(n>=80){
+        printf("%c",'B');
+    }
+    else if(n>=70){
+        printf("%c",'C');
+    }
+    else id(n>=60){
+        printf("%c",'D');
+    }
+    else{
+        printf("%c",'F');
+    }
+    return 0;
+}
