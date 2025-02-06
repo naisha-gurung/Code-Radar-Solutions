7b68640de9094c40e88a8209917cd9b33f7a92d2
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-06 17:33:03 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -7,14 +7,14 @@ int main() {
     for(int i=0;i<2;i++){
         for(int j=i+1;j<3;j++){
             printf("%d %d %d\n",arr[i],arr[j],arr[3-(i+j)]);
-            // if arr[i]+arr[j]<arr[3-(i+j)]{
-            //    // printf("%s","Invalid");
-            //     return 0;
-            // }
+            if (arr[i]+arr[j]<arr[3-(i+j)]){
+               printf("%s","Invalid");
+                return 0;
+            }
         }
 
 
     }
-    //printf("%s","Valid");
+    printf("%s","Valid");
     return 0;
 }
