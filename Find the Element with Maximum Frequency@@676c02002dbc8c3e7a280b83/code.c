07c687e4d9b2e5dd5fa76a4faf17397ec88592d1
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-07 11:26:14 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
-// bool check(int n;int arr[][],int j){
+bool check(int n;int arr[][],int j){
 //     for(int i=0;i<j;i++){
 //         if(arr[i][0]==n){
 //             return true;
 //         }
 //     }
-//     return false;
-// }
+    return false;
+}
 int main() {
       int n;
     scanf("%d",&n);
@@ -16,20 +16,20 @@ int main() {
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    // int count=0;
-    // int j=0;
-    // for(int i=0:i<n;i++){
-    //     if check(arr[i],arr2,j){
-    //             continue;
-    //         }
-    //     for(int j=0;j<n;j++){
-    //         if(arr[i]==arr[j]){
-    //             ++count;
-    //         }
-    //     }
-    //     arr2[j][1]=count;
-    //     arr2[j][0]=arr[i];
-    //     ++j;
-    // }
+    int count=0;
+    int j=0;
+    for(int i=0:i<n;i++){
+        if check(arr[i],arr2,j){
+                continue;
+            }
+        for(int j=0;j<n;j++){
+            if(arr[i]==arr[j]){
+                ++count;
+            }
+        }
+        arr2[j][1]=count;
+        arr2[j][0]=arr[i];
+        ++j;
+    }
     return 0;
 }
