3a2d64791816e8bf18bb5ea13979cdf813314a95
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-08 10:31:58 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -3,29 +3,29 @@ int main() {
     int n;
     scanf("%d",&n);
     int arr[n];
-    int a,b,;
+    int a,b;
     for(int i=0;i<n;i++){
         scanf("%d ",&arr[i]);
     }
     
-    // int min=arr[0];
-    // for(int i=0;i<n-1;i++){
-    //     for(int j=0;j<n-i-1;j++){
-    //         if(arr[j]>arr[j+1]){
-    //             int a=arr[j];
-    //             arr[j]=arr[j+1];
-    //             arr[j+1]=arr[j];
-    //         }
+    int min=arr[0];
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int a=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=arr[j];
+            }
 
-    //     }
-    // }
-    // for(int i=1;i<n;i++){
-    //     if((arr[i]-arr[i-1])<min){
-    //         min=arr[i]-arr[i-1];
-    //         a=arr[i];
-    //         b=arr[i-1];
-    //     }
-    // }
-    // printf("%d %d",a,b);
+        }
+    }
+    for(int i=1;i<n;i++){
+        if((arr[i]-arr[i-1])<min){
+            min=arr[i]-arr[i-1];
+            a=arr[i];
+            b=arr[i-1];
+        }
+    }
+    printf("%d %d",a,b);
     return 0;
 }
