c572fde286594967142346df08077c129449cd5c
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-08 14:32:32 +0530

    Solution to Find and Display the Top Scorer

---
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a;
+    float c;
+    char b[n][50];
+    for(int i=0;i<n;i++){
+       scanf("%d %s %f",&a ,&b[i],&c );
+    }
+    int max=arr[0];
+    index=0;
+    for(int i=0;i<n;i++){
+        if(max<arr[i]){
+            max=arr[i];
+            inex=i;
+        }
+    }
+      printf("Top Scorer: Roll Number: %d, Name: %s, Marks: %.2f\n",a,b[index],c);
+    return 0;
+}
