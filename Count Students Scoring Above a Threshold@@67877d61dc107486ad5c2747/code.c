e0f111da0b0f5dc8b948eef656abe1425a889ce3
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-09 10:36:49 +0530

    Solution to Count Students Scoring Above a Threshold

---
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct st{
+    int a;
+    float c;
+    char b[50];
+};
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct st s[n];
+    for(int i=0;i<n;i++){
+        scanf("%d %s %f",&s[i].a;&s[i].b;&s[i].c);
+    }
+    int m;
+    scanf("%d",&m);
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(s[i].c>=m){
+            ++count;
+        }
+    }
+    printf("Count of students scoring above %.2f: %d",m,count);
+       return 0;
+}
