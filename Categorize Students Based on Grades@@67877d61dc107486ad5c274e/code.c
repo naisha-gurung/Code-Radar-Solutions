ad382454bf5946346d6bd04e9d38650b9b8a1bdb
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-09 11:30:37 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -12,23 +12,23 @@ int main() {
     for(int i=0;i<m;i++){
         scanf("%d %s %f",&s[i].a,&s[i].b,&s[i].c);
     }
-for(int i=0;i<m;i++){
-    float n=s[i].c;
-     if (n>=90.0){
-        s[i].d='A';
-    }
-    else if(n>=80.0){
-       s[i].d='B';
-    }
-    else if(n>=70.0){
-       s[i].d='C';
-    }
-    else if(n>=60.0){
-        s[i].d='D';
-    }
-    else{
-      s[i].d='F';
-    }
-}
+// for(int i=0;i<m;i++){
+//     float n=s[i].c;
+//      if (n>=90.0){
+//         s[i].d='A';
+//     }
+//     else if(n>=80.0){
+//        s[i].d='B';
+//     }
+//     else if(n>=70.0){
+//        s[i].d='C';
+//     }
+//     else if(n>=60.0){
+//         s[i].d='D';
+//     }
+//     else{
+//       s[i].d='F';
+//     }
+// }
     return 0;
 }
