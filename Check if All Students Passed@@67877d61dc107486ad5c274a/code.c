4d7cc71064d237abe5bfc06b7b1dfd58aa1017de
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-09 10:57:55 +0530

    Solution to Check if All Students Passed

--- Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -12,7 +12,7 @@ int main() {
         scanf("%d %s %f",&s[i].a,&s[i].b,&s[i].c);
     }
     for(int i=0;i<n;i++){
-        if(s[i].c<=50){
+        if(s[i].c<=50.0){
             printf("Not All Passed");
             return 0;
         }
