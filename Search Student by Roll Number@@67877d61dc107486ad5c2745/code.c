dcfb2c80cddf957a562e7f4e7de130574ab80ee8
naisha-gurung <naisha.gurung03@gmail.com>
2025-02-09 10:26:13 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -15,8 +15,8 @@ int main() {
     scanf("%d",&y);
     for(int i=0;i,n;i++){
         if(y==s[i].rn){
-            x=y;
-             printf("Roll Number: %d, Name: %s, Marks: %.2f",s[x].rn,s[x].name,s[x].m);
+            // x=y;
+            //  printf("Roll Number: %d, Name: %s, Marks: %.2f",s[x].rn,s[x].name,s[x].m);
              return 0;
         }
     }
