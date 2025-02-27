0eaecf5535cba67635b72501d9434a010729fc8a
Naisha Gurung <naisha.gurung03@gmail.com>
2025-02-27 19:06:21 +0530

    Solution to Check if Two Strings are Anagrams

---
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -0,0 +1,63 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char s[50],st[50];
+    char ans1[50][2];
+    char ans2[50][2];
+    int k=0;
+    fgets(s,sizeof(s),stdin);
+    fgets(s2,sizeof(st),stdin);
+    if(strlen(s)!=strlen(s2)){
+        printf("No");
+        return 0;
+    }
+     for(int i=0;i<strlen(s);i++){
+        if (!(strchr(st,s[i]))){
+            int count=0;
+            for( int j=i+1;j<strlen(s);j++){
+                if(s[j]==s[i]){
+                    ++count;
+                }
+            }
+            ans[k][0]=s[i];
+            ans[k][1]=count;
+            ++k;
+            st[strlen(st)] = s[i]; 
+            st[strlen(st) + 1] = '\0';
+        }
+    }
+    for(int i=0;i<strlen(s2);i++){
+        if (!(strchr(st,s2[i]))){
+            int count=0;
+            for( int j=i+1;j<strlen(s2);j++){
+                if(s2[j]==s2[i]){
+                    ++count;
+                }
+            }
+            ans2[k][0]=s2[i];
+            ans2[k][1]=count;
+            ++k;
+            st[strlen(st)] = s2[i]; 
+            st[strlen(st) + 1] = '\0';
+        }
+    }
+    int rows = sizeof(ans) / sizeof(ans[0]);       
+    int cols = sizeof(ans[0]) / sizeof(ans[0][0]);
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<rows;j++){
+            if(ans[i][0]==ans2[j][0]){
+                if (ans[i][1]!=ans2[j][1]){
+                    printf("No");
+                    return 0;
+                }
+            }
+        }
+    }
+        printf("Yes");
+    
+    
+    return 0;
+}
+
+
