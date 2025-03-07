f0a8ffc8bd181243c878d42b1e268d88afcf4143
Naisha Gurung <naisha.gurung03@gmail.com>
2025-03-07 20:16:01 +0530

    Solution to Convert to Uppercase

---
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char word[50];
+    scanf("%s",&word);
+    char ans[50];
+    for(int i=0;i<strlen(word);i++){
+        if (int(word[i])<123 && int(word[i])>97){
+            char temp[2];
+            temp[0]=char(int(word[i])+32);
+            temp[1]='\0';
+            strcat(ans,temp);
+        }
+    }
+    return 0;
+}
