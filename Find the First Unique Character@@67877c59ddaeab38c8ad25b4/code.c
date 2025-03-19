d60ac5a1a3977fd84dc49410596e103c012de393
Naisha Gurung <naisha.gurung03@gmail.com>
2025-03-19 18:36:29 +0530

    Solution to Find the First Unique Character

---
+++ Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
@@ -0,0 +1,29 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char word[50];
+    scanf("%s",word);
+    char temp [50];
+    int c;
+    for(int i=0;i<strlen(word);i++){
+        c=1;
+        if(!(strchr(temp,word[i]))){
+            for(int j=i+1;j<strlen(word);j++){
+                if(word[i]==word[j]){
+                    char temp2[2];
+                    temp2[0]=word[i];
+                    temp2[1]='\0';
+                    strcat(temp,temp2);
+                    c=0;
+                }
+            }
+        }
+        if(c){
+            printf("%c",word[i]);
+            return 0;
+        }
+    }
+    printf("-");
+    return 0;
+}
