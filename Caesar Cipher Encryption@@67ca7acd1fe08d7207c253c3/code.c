8ecba06646fc5cc1fa3c56e5d666df3c2389d443
Naisha Gurung <naisha.gurung03@gmail.com>
2025-03-20 17:13:02 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,8 +1,8 @@
 // Your code here...
 void caesarCipher(char message[] ,int shift ,char encrypted[]){
     for(int i=0;i<strlen(message);i++){
-        if(isalpha(message[i])){
+        //if(isalpha(message[i])){
       //  encrypted[i]=message[i]+shift;
-        }
+       // }
     }
 }
