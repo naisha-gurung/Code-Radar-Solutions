3df0d4cfbdc79cd32e32ce68a6c6394778b6d636
Naisha Gurung <naisha.gurung03@gmail.com>
2025-03-31 20:54:04 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+
+//if number is present in the array returns false 
 bool check(int arr2[],int temp,int n){
     if (n==0){
         return true;
@@ -21,13 +23,17 @@ int main() {
     }
     for (int i=0;i<n;i++){
         count=0;
+        //if number is not in arr2 check func will return true and count the freq
        if (check(arr2,arr[i],x)){
+
+        //Calculate freq
          for(int j=0;j<n;j++){
          if (arr[i]==arr[j]){
              count+=1;
          }
          }
         printf("%d %d\n",arr[i],count);
+
         }
         arr2[x]=arr[i];
          x++;
