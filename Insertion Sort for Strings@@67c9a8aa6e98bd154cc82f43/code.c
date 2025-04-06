4f4930251bf083eab15ec97a5b31f6f1ba4aeeac
Naisha Gurung <naisha.gurung03@gmail.com>
2025-04-06 19:34:39 +0530

    Solution to Insertion Sort for Strings

--- Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -7,21 +7,21 @@ void insertionSort(char arr[][100],int n){
        strcpy(curr,arr[i]);
         prev=i-1;
         //printf("%s\n",curr);
-        while(prev>=0 && arr[prev][0]>curr){
-            printf("%c %c\n",arr[prev][0],arr[prev+1][0]);
+        while(prev>=0 && arr[prev][0]>curr[0]){
+            // printf("%c %c\n",arr[prev][0],arr[prev+1][0]);
             
             // printf("%s %s\n",arr[prev],arr[prev+1]);
             strcpy(arr[prev+1],arr[prev]);
             prev--;
-            printf("%s %s\n",arr[prev],arr[prev+1]);
-            printf("%d\n",prev);
+            // printf("%s %s\n",arr[prev],arr[prev+1]);
+            // printf("%d\n",prev);
         }
-         printf("%s\n",arr[prev+1]);
+        //  printf("%s\n",arr[prev+1]);
         strcpy(arr[prev+1],curr);
-        printf("\n");
-    for(int i=0;i<n;i++){
-        printf("%s ",arr[i]);
-    }
+    //     printf("\n");
+    // for(int i=0;i<n;i++){
+    //     printf("%s ",arr[i]);
+    // }
     }
     
 }
